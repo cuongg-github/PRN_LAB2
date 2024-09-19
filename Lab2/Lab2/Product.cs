3bfd0cc8304be11d3eb630aebdcb6db66e2781a1
cuongg-github <qc01696@gmail.com>
2024-09-19 17:44:25 +0700

    Add files via upload

---
+++ Lab2/Lab2/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Product
+    {
+        private string product_name;
+        private double price;
+        private int discount;
+        public Product() { }
+        //3 parameter
+        public Product (string product_name, double price, int discount )
+        {
+            this.product_name = product_name;
+            this.price = price;
+            this.discount = discount;
+        }
+        //2 parameter
+        public Product(string product_name, double price)
+        {
+            this.product_name= product_name;
+            this.price = price;
+            this.discount = 0;
+        }
+        public string Product_Name { get; set; }
+        public double Price { get; set; }
+        public int Discount { get; set; } 
+        
+        private double getImportTax()
+        {
+            return (price * 10) / 100;
+        }
+
+        public void input()
+        {
+            Console.WriteLine("Please enter product's name : ");
+            this.product_name = Console.ReadLine();
+            Console.WriteLine("Please enter product's price : ");
+            this.price = double.Parse( Console.ReadLine() );
+            Console.WriteLine("Please enter product's discount :" );
+            this.discount = int.Parse( Console.ReadLine() );
+        }
+        public void display()
+        {
+            Console.WriteLine($"Product name : {product_name}");
+            Console.WriteLine($"Product price : {price}");
+            Console.WriteLine($"Product discount : {discount}");
+            Console.WriteLine($"Product tax : {getImportTax()}");
+        }
+
+    }
+}
---
+++ Lab2/Lab2/main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            /*2. Write a program that creates 2 products whose information is entered from the keyboard,
+            then calls the display method to display the information of the 2 created product objects.
+            Product product1 = new Product();
+            Product product2 = new Product();
+            product1.input();
+            product2.input();
+            product1.display();
+            product2.display();*/
+            /*3.Write a program that creates 2 products, one with discount and one without discount,
+            then displays the information of the 2 products on the screen.
+            Product product1 = new Product("iphone",800000,10);
+            Product product2 = new Product("android",500000);
+            product1.display();
+            product2.display();*/
+
+
+}
+}
+}
